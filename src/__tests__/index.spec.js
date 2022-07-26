6e284390024c8d93dba4ead558e53de9d5602d0d
rdolgopolsky <rdolgopolsky@gmail.com>
2022-07-26 18:36:05 +0300

    Returned Default

--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -7,29 +7,33 @@ describe('Hotel', () => {
     expect(hotel.bankAccount).toStrictEqual(0);
     expect(hotel.guests).toStrictEqual({});
   });
-  
+
   describe('checks methods', () => {
     it('getLength()', () => {
       expect(hotel.getLength()).toStrictEqual(0);
     });
-    
+
     it('getActualFreePlace()', () => {
       expect(hotel.getActualFreePlace()).toStrictEqual(30);
     });
-    
+
     it('checkInGuest()', () => {
       const guest = hotel.checkInGuest('Bohdan', 'Onatskyi', 40);
       expect({ ...guest }).toStrictEqual({
-        firstName: 'Bohdan', lastName: 'Onatskyi', money: 20
+        firstName: 'Bohdan',
+        lastName: 'Onatskyi',
+        money: 20,
       });
       expect(hotel.getLength()).toStrictEqual(1);
       expect(hotel.bankAccount).toStrictEqual(20);
-      
+
       const guestWithoutMoney = hotel.checkInGuest('John', 'Doe', 12);
-      expect(guestWithoutMoney).toStrictEqual('Sorry, you have not enough money');
+      expect(guestWithoutMoney).toStrictEqual(
+        'Sorry, you have not enough money'
+      );
       expect(hotel.getLength()).toStrictEqual(1);
       expect(hotel.bankAccount).toStrictEqual(20);
-      
+
       new Array(29)
         .fill({ firstName: 'test', lastName: 'test', money: 20 })
         .forEach((item) => {
@@ -37,7 +41,9 @@ describe('Hotel', () => {
         });
       expect(hotel.getLength()).toStrictEqual(30);
       expect(hotel.bankAccount).toStrictEqual(20 * 30);
-      expect(hotel.checkInGuest('Man', 'WithMillionDollar', 1000000)).toStrictEqual('Sorry, we have not free spaces');
+      expect(
+        hotel.checkInGuest('Man', 'WithMillionDollar', 1000000)
+      ).toStrictEqual('Sorry, we have not free spaces');
     });
   });
 });
