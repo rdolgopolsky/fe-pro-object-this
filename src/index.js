6a7d84b5f68a917f08b6b4c1fb8215bedaf95402
rdolgopolsky <rdolgopolsky@gmail.com>
2022-07-26 18:40:48 +0300

    run prettier

--- src/index.js
+++ src/index.js
@@ -3,36 +3,36 @@ export const hotel = {
   priceByPlace: 20,
   bankAccount: 0,
   guests: {},
-  
+
   getLength() {
     return Object.keys(this.guests).length;
   },
-  
+
   getActualFreePlace() {
     return this.quantityOfPlaces - this.getLength();
   },
-  
+
   paidPerPlace() {
-    return this.bankAccount += this.priceByPlace;
+    return (this.bankAccount += this.priceByPlace);
   },
-  
+
   checkInGuest(guestName, guestLastName, guestMoney) {
     if (!this.getActualFreePlace() > 0) {
       return 'Sorry, we have not free spaces';
     }
-    
+
     if (guestMoney < this.priceByPlace) {
       return 'Sorry, you have not enough money';
     }
-    
+
     guestMoney -= this.priceByPlace;
-    
+
     this.guests[this.getLength()] = {
       firstName: guestName,
       lastName: guestLastName,
-      money: guestMoney
+      money: guestMoney,
     };
-    
+
     this.paidPerPlace();
-  }
+  },
 };
